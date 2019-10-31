8afb1c40daf858a3a03fbada468d10401bf439d3
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-10-30 23:51:43 -0700

    Add info command

---
+++ packages/cli/src/commands/info.ts
@@ -0,0 +1,26 @@
+import { Command } from '@oclif/command';
+
+export default class Info extends Command {
+  static description = 'Gather relevant information about the CLI';
+
+  async run() {
+    const envinfo = require('envinfo');
+
+    envinfo
+      .run(
+        {
+          System: ['OS', 'CPU'],
+          Binaries: ['Node', 'Yarn', 'npm'],
+          Browsers: ['Chrome', 'Edge', 'Firefox', 'Safari'],
+          npmPackages: '/**/{typescript,@straw-hat/*/}',
+          npmGlobalPackages: ['@straw-hat/cli'],
+        },
+        {
+          showNotFound: true,
+          duplicates: true,
+          fullTree: true,
+        }
+      )
+      .then(console.log);
+  }
+}

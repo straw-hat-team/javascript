eecf48a37a65240e49cfd8a4b87136dc3293514b
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-10-30 22:19:53 -0700

    Remove dead code

--- scripts/copyFiles.js
+++ scripts/copyFiles.js
@@ -1,11 +1,9 @@
 /* eslint-disable no-console */
 const path = require('path');
 const fse = require('fs-extra');
-const glob = require('glob');
 
 const packagePath = process.cwd();
 const buildPath = path.join(packagePath, './dist');
-const srcPath = path.join(packagePath, './src');
 
 async function includeFileInBuild(file) {
   const sourcePath = path.resolve(packagePath, file);
@@ -19,43 +17,6 @@ async function includeFileInBuild(file) {
 
   await fse.copy(sourcePath, targetPath);
   console.log(`Copied ${sourcePath} to ${targetPath}`);
-}
-
-async function createModulePackages({ from, to }) {
-  const directoryPackages = glob.sync('*/index.js', { cwd: from }).map(path.dirname);
-
-  await Promise.all(
-    directoryPackages.map(async directoryPackage => {
-      const packageJson = {
-        sideEffects: false,
-        module: path.join('../esm', directoryPackage, 'index.js'),
-        typings: './index.d.ts',
-      };
-      const packageJsonPath = path.join(to, directoryPackage, 'package.json');
-
-      const [typingsExist] = await Promise.all([
-        fse.exists(path.join(to, directoryPackage, 'index.d.ts')),
-        fse.writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2)),
-      ]);
-
-      if (!typingsExist) {
-        throw new Error(`index.d.ts for ${directoryPackage} is missing`);
-      }
-
-      return packageJsonPath;
-    }),
-  );
-}
-
-async function typescriptCopy({ from, to }) {
-  if (!(await fse.exists(to))) {
-    console.warn(`path ${to} does not exists`);
-    return [];
-  }
-
-  const files = glob.sync('**/*.d.ts', { cwd: from });
-  const cmds = files.map(file => fse.copy(path.resolve(from, file), path.resolve(to, file)));
-  return Promise.all(cmds);
 }
 
 async function createPackageFile() {
@@ -102,10 +63,6 @@ async function run() {
   try {
     await createPackageFile();
     await copySupportFiles();
-    // TypeScript
-    await typescriptCopy({ from: srcPath, to: buildPath });
-
-    await createModulePackages({ from: srcPath, to: buildPath });
   } catch (err) {
     console.error(err);
     process.exit(1);

f58d21120d60fb1f6381020cf6287f2cc300dfe8
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-11-27 06:47:48 -0800

    Remove babel and typescript eslint configs

--- packages/tooling/eslint-config-base/index.js
+++ packages/tooling/eslint-config-base/index.js
@@ -1,32 +1,5 @@
 module.exports = {
   extends: ['airbnb-base', '@straw-hat/eslint-config-prettier'],
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
-    },
-  },
-  parser: 'babel-eslint',
-  overrides: [
-    {
-      files: ['**/*.ts?(x)'],
-      extends: [
-        'plugin:@typescript-eslint/eslint-recommended',
-        'prettier/@typescript-eslint',
-        'plugin:import/typescript',
-      ],
-      parser: '@typescript-eslint/parser',
-      parserOptions: {
-        warnOnUnsupportedTypeScriptVersion: true,
-      },
-      rules: {
-        // Fixes definition files imports
-        'spaced-comment': ['error', 'always', { markers: ['/'] }],
-        'no-unused-vars': 'off',
-      },
-    },
-  ],
   rules: {
     'import/prefer-default-export': 'off',
     'import/no-extraneous-dependencies': [

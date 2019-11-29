b7e4d4a3dc61653b056fe290eef4b1f34467ef7d
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-11-29 01:57:18 -0800

    Improve types

--- packages/tooling/cli/src/types.ts
+++ packages/tooling/cli/src/types.ts
@@ -1,5 +1,18 @@
+import { GlobalConfig } from '@jest/types/build/Config';
+
 export type Environment = 'development' | 'test' | 'production' | 'e2e';
 
-export type ShcConfig = {};
+export type ShcConfig = {
+  jestConfig?(config: GlobalConfig): GlobalConfig;
+};
 
 export type ShcConfigFactory = (params: { env: Environment }) => ShcConfig;
+
+export type PackageJSON = {
+  name: string;
+  version: string;
+  dependencies?: Record<string, string>;
+  devDependencies?: Record<string, string>;
+  scripts?: Record<string, string>;
+  homepage?: string;
+};

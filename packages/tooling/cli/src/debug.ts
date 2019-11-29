714be40494bc1af9ce05e1500a455664d89a912c
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-11-29 01:57:05 -0800

    Change debug factory name

--- packages/tooling/cli/src/debug.ts
+++ packages/tooling/cli/src/debug.ts
@@ -1,6 +1,6 @@
 import debug from 'debug';
 
-export function createDebug(...scope: string[]) {
+export function createDebugger(...scope: string[]) {
   const namespace = ['@straw-hat/cli', ...scope].join(':');
   return debug(namespace);
 }

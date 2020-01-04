bbbae921186da5f46768cb6f357557c1edeb3d82
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2020-01-03 16:20:22 -0800

    Fix eslint config

--- packages/eslint/eslint-config-base/lib/index.js
+++ packages/eslint/eslint-config-base/lib/index.js
@@ -1,6 +1,17 @@
 module.exports = {
   extends: ['airbnb-base', '@straw-hat/eslint-config-prettier'],
   rules: {
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        mjs: 'never',
+        jsx: 'never',
+        tsx: 'never',
+        ts: 'never',
+      },
+    ],
     'import/prefer-default-export': 'off',
     'import/no-extraneous-dependencies': [
       'error',
